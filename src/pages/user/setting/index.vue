0c0089384fd4ae2827c8e01cfd12156707b17831
onechuan <oenchuan2020@163.com>
2022-07-14 22:21:51 +0800

    feat: fix vue

--- src/pages/user/setting/index.vue
+++ src/pages/user/setting/index.vue
@@ -1,13 +1,7 @@
 <template>
-    <div>
-        setting
-    </div>
+  <div> setting </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
